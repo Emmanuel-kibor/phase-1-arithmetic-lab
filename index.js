e9f6e177d3c3bbe15c43d65f4bb0fd7d94036fee
emmanuel <emmanuelkibor33@gmail.com>
2022-11-30 14:16:36 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
-// Write your code here
+// Multiplication Code
+const num1 = 31;
+const num2 = 2;
+
+const multiply = num1 * num2
+
+console.log (num1 * num2);
+
+let integer = 1
+const random = 1
+
+const num3 = 8
+const num4 = 2
+let mod = num3 / num4
+
+let max = Math.max(20)
